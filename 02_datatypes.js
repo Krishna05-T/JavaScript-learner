fc16e1354dca3b1470441f671a34254c0ee83b2e
Krishna Tirole <krishnatirole05@gmail.com>
2025-12-15 11:50:13 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello");       // we use node js not browser
+
+let name = "Krishna" // string
+let age = 19;       //number
+let isLogIn = true;     //boolean
+let bigno = 123456789n; // bigint
+let x;      //undefined
+let y = null;       //null
+// symbol => used for unique identifiers
+
+console.log(typeof name);
+
+console.log(typeof age);
+console.log(typeof y);
+console.log(typeof x);
