1bc7e6dbb79dc52b29dd77a0cef99e7f0b68f213
Krishna Tirole <krishnatirole05@gmail.com>
2025-12-15 14:27:23 +0530

    Control Flow

--- 16_control_flow.js
+++ 16_control_flow.js
@@ -24,7 +24,7 @@ if(score > 50) {
     console.log(`power is : ${power}`);
 }
 
-console.log(`power is : ${power}`);     // error power is not defined
+//console.log(`power is : ${power}`);     // error power is not defined
 
 // short hand if 
 
@@ -62,3 +62,26 @@ if(userLoggedIN || loggedInFormEmail){
     console.log("User logged in");
     
 }
+
+// ternary operator
+
+let price = 100;
+
+price > 80 ? console.log("More than 80"): console.log("Less than 80");
+
+// nullish coalescing operator
+
+// The ?? operator provides a default value when a variable is null or undefined.
+
+let name = null;
+
+name = name ?? "Krishan"
+console.log(name);  // Output: Krishan
+
+// Because name is null, "Krishna" is used.
+
+let num;
+num = num ?? 10 ?? 20
+console.log(num);  // Output: 10
+
+
