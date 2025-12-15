f9f4a16352fd3ad54893597a3efa12feb0672119
Krishna Tirole <krishnatirole05@gmail.com>
2025-12-15 11:49:09 +0530

    variables in js

---
+++ 01_variable.js
@@ -0,0 +1,17 @@
+const accountNo = 342556;
+let email = "krishna05@gmail.com";
+var accountPassword = 1234;
+city = "Indor";
+let state;
+
+// accountNo = 34;  //it constant so it is not change
+console.log(accountNo);
+email = "krishna07@google.com";
+accountPassword = 93432;
+city = "Bengaluru";
+
+    /*
+        not use var because it is unsafe
+    */
+
+console.table([accountNo, email, accountPassword, city, state]);
