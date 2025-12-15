ae91d207bd5cdb7fc1998652862bf573d5f83575
Krishna Tirole <krishnatirole05@gmail.com>
2025-12-15 11:59:15 +0530

    comparisons

---
+++ 05_comparsion.js
@@ -0,0 +1,38 @@
+        // comparsion operator
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+            // relational operator
+// console.log(true && false);
+// console.log(true || false);
+// console.log(!true);
+
+
+// console.log("2" == 2);
+// console.log("02" > 1);       // not predictable
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);      // null and comparsion work different
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0); 
+
+
+// console.log("5"  == 5);
+// console.log("5" === 5);          // === use for strick check it give ture on same data type
+
+
+
+
+
+
+
+
+
