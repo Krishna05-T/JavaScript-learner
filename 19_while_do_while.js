7f3aa2c3e1e93d6eb19a3107435dc656a7dfadca
Krishna Tirole <krishnatirole05@gmail.com>
2025-12-15 16:40:25 +0530

    While | do while

---
+++ 19_while_do_while.js
@@ -0,0 +1,26 @@
+// while loop
+
+let i =1; 
+
+while (i<=10) {
+    console.log(i);
+    i++;
+}
+
+let myArr = ["C", "C++", "Java", "JavaScript", "Python"];
+
+let index = 0;
+ while (index< myArr.length) {
+    console.log(myArr[index]);
+    index++;
+ }
+
+ // do while loop
+
+let score = 11;
+
+do{
+    console.log(score);     // it run atleast when condition is false
+    score++;
+}while(score<=10)
+
