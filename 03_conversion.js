1f0b9ffc9ec9f6bdd63f6ce76c34a20ed7cb143b
Krishna Tirole <krishnatirole05@gmail.com>
2025-12-15 11:58:17 +0530

    conversion and operations

---
+++ 03_conversion.js
@@ -0,0 +1,41 @@
+let score = null;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let scoreInNumber = Number(score);
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber);
+
+
+// "33" => 33 string to no. converted
+// "33abc" => NaN
+// null => 0
+// true => 1 , false => 0
+// undefined => NaN
+// "hello" => NaN 
+
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false , "krishna" => true
+// null => false
+// undefined => false
+// " " => true 
+
+let someno = 33;
+let stringNo = String(someno);
+console.log(stringNo);
+console.log(typeof stringNo);
+
+ // 33 => "33"
+// true => "true"
+// false => "false"
+// null => "null"
+// undefined => "undefined"
+
---
+++ 04_operation.js
@@ -0,0 +1,34 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**3);
+console.log(2%3);
+
+let str1 = "hello";
+let str2 = " krishna";
+
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 4;
+console.log(num2);
+
+let x =3;
+const y = x++;
+
+console.log(`x:${x}  y:${y}`);
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a} b:${b}`);
