91938391d96f560ff6e76169d22824baa4f8cd19
Krishna Tirole <krishnatirole05@gmail.com>
2025-12-15 13:13:37 +0530

    Control Flow

---
+++ 16_control_flow.js
@@ -0,0 +1,64 @@
+const isUserLoggedIn = true;
+
+if(isUserLoggedIn){
+    console.log("Welcome back, user!");  
+}
+
+// operator in JS
+
+//<, >, <=, >=, ==, ===, !=, !==
+
+if (2 === "3") {
+    console.log("Condition is true");
+} else {
+    console.log("Condition is false");
+}
+
+
+// scope 
+
+let score = 100;
+
+if(score > 50) {
+    let power = "fly";
+    console.log(`power is : ${power}`);
+}
+
+console.log(`power is : ${power}`);     // error power is not defined
+
+// short hand if 
+
+let age = 18
+if(age >= 18) console.log("You are eligible to vote");
+
+
+// multiple conditions
+
+const balace = 1200;
+
+if(balace < 500) {
+    console.log("less than 500");
+} else if(balace < 750) {
+    console.log("less than 750");
+} else if(balace < 900) {
+    console.log("less than 900");
+}else {
+    console.log("More than 1000`");
+}
+
+
+// multiple conditions 
+
+let userLoggedIN = true
+let debitcard = true;
+let loggedInFromGoogel = true;
+let loggedInFormEmail = false;
+
+if(userLoggedIN && debitcard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if(userLoggedIN || loggedInFormEmail){
+    console.log("User logged in");
+    
+}
